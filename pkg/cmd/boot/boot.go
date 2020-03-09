c29c1e35074b5f659609a1384b347365bf0b3899
James Strachan <james.strachan@gmail.com>
2020-03-09 13:46:00 +0000

    fix: improve logging for k8s connectivity
    Signed-off-by: James Strachan <james.strachan@gmail.com>

--- pkg/cmd/boot/boot.go
+++ pkg/cmd/boot/boot.go
@@ -597,6 +597,8 @@ func (o *BootOptions) verifyClusterConnection() error {
 	}
 
 	if err != nil {
+		log.Logger().Warnf("failed to list namespaces to check kubernetes cluster connectivity: %s", err.Error())
+
 		return fmt.Errorf("You are not currently connected to a cluster, please connect to the cluster that you intend to %s\n"+
 			"Alternatively create a new cluster using %s", util.ColorInfo("jx boot"), util.ColorInfo("jx create cluster"))
 	}
